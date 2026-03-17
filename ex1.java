78719770210b79408ae74df49ed25027a6e0b4fb
balajim0911 <balajimaddisetty9@gmail.com>
2026-03-17 12:10:19 +0530

    Add files via upload

---
+++ ex1.java
@@ -0,0 +1,17 @@
+//using scanner 
+import java.util.Scanner;
+public class ex1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.println("Hello, " + name + "! You are " + age + " years old.");
+
+        scanner.close();
+    }
+}
